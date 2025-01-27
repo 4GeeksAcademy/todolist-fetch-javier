3966b89b0f8f9f2e4fdac0636ad51e651a85c16f
Javier de los Ángeles <javier.delosangeles@gmail.com>
2025-01-27 09:03:19 +0000

    funciones

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,107 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+
 
 //create your first component
 const Home = () => {
+	const [tasks, setTasks] = useState([]);
+	const [inputValue, setInputValue] = useState("");
+	const [loading, setLoading] = useState("true");
+	const [todos, setTodos] = useState([]);
+
+
+	const loadTodos = async () => {
+		try{
+			setLoading(true);
+			const response = await fetch("https://playground.4geeks.com/todo/users/javier")
+			console.log(response);
+			if(!response.ok) createUser();
+			const fetchData = await response.json();
+			console.log(fetchData);
+			setTasks(fetchData.todos);
+			setLoading(false);
+		} catch(error){
+			console.log("Algo fue mal " + error);
+		}
+	}
+	const createUser = async () => {
+		try{
+			const response = await fetch("https://playground.4geeks.com/todo/users/javier", {
+				method: "POST"
+			})
+			console.log(response);
+		} catch (error){
+			console.log("Algo fue mal "+ error);
+		}
+	}
+
+	const addTodo = async (todo) => {
+		try{
+			const newTodo = {
+				label: todo,
+				done: false,
+			}
+
+			const response = await fetch("https://playground.4geeks.com/todo/users/javier", {
+				method: "POST",
+				body: JSON.stringify(newTodo),
+				headers: {
+					"Content-Type": "application/json"
+				}
+			})
+			console.log(response);
+			if(!response.ok) throw new Error("HTTP error "+ response.statusText);
+
+			loadTodos();
+		} catch(error){
+			console.log(error);
+		}
+	}
+
+	const removeTodo = async (todoId) => {
+		try{
+			const response = await fetch("https://playground.4geeks.com/todo/todos/${todoId}", {
+				method: "DELETE",
+				headers: {
+					"Content-Type": "application/json"
+				}
+			})
+
+			if(!response.ok) throw new Error("HTTP error "+ response.statusText)
+		} catch (error){
+	console.log(error);
+	}
+	}
+
+	useEffect(() => {
+		loadTodos();
+	}, [])
+
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div classname="container">
+			<h1>Todos</h1>
+			<ul>
+				<li><input 
+				        type="text"
+				        onChange={(e) => setInputValue(e.target.value)} 
+						value={inputValue} 
+						onKeyDown={(e) => {
+							if (e.key === 'Enter') {
+							  setTodos([...todos, inputValue]);
+							  setInputValue("");
+							}}}
+						placeholder="What needs to be done?">
+					</input>
+				</li>
+				{todos.map((tarea, index) => (
+					<li key={index} >
+						{/*Filtro de todos los index menos el que se hace click */}
+						{tarea} <button onClick={() => setTodos(todos.filter((t, currentIndex) => index != currentIndex ))}>Eliminar</button>
+					</li>
+				))}
+
+			</ul>
+			<div>{todos.length} item left</div>
 		</div>
 	);
 };
